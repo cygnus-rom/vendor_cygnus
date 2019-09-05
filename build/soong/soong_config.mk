386f12214898c6a6626dfad6644a910219ffe900
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    cygnus: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,10 @@
-_contents := $(_contents)    "Cygnus":{$(newline)
+_json_contents := $(_json_contents)    "Cygnus":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Target_uses_color_metadata,            $(filter true,$(TARGET_USES_COLOR_METADATA)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
