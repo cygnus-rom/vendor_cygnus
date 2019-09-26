77fa8905f879d71044b00858d73129b18960e0f1
Jake Weinstein <jake@aospa.co>
2019-09-26 13:09:03 +0530

    vendor: enable Snapdragon LLVM 8.0 Compiler
    passes CTS and has no stability issues.
    
    Change-Id: I6d46ef2c208fecb8c8032c5ceb3bec09bc6e74b4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/cygnus/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/cygnus/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/cygnus/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/cygnus/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
