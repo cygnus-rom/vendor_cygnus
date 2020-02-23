ReloadedOS/vendor_reloaded

vendor: added APN config files to prebuilt
Change-Id: Id6d84a1698b7c4e6e58170c93cd6a61e49bda966

configs/common.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     vendor/wave/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     vendor/wave/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Build Snapdragon apps
 PRODUCT_PACKAGES += \
     SnapdragonGallery \

cygnus-rom/vendor_cygnus

configs/common.mk
@@ -67,3 +67,7 @@ include vendor/cygnus/configs/props.mk
 
 # Version
 $(call inherit-product, vendor/cygnus/configs/versioning.mk)
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/cygnus/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml