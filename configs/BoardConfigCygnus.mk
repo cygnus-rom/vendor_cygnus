bionicBUG/vendor_cygnus-caf

cygnus: Dynamically generate kernel headers using pa generator
Add a soong vendor plugin for kernel config variables so we can use
these in go.

Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

configs/BoardConfigCygnus.mk
@@ -1,2 +1,5 @@
 # Kernel
 include vendor/cygnus/configs/BoardConfigKernel.mk
+
+# Soong
+include vendor/cygnus/config/BoardConfigSoong.mk

cygnus-rom/vendor_cygnus

configs/BoardConfigCygnus.mk
@@ -1,2 +1,5 @@
 # Kernel
 include vendor/cygnus/configs/BoardConfigKernel.mk
+
+# Soong
+include vendor/cygnus/configs/BoardConfigSoong.mk