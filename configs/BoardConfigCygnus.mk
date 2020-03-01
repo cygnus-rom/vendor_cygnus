8cb4be7ed878aef21517c5da00204eab8b9b8919
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-01 16:18:40 +0000

    vendor: Allow providing camera server and service

--- configs/BoardConfigCygnus.mk
+++ configs/BoardConfigCygnus.mk
@@ -2,4 +2,11 @@
 include vendor/cygnus/configs/BoardConfigKernel.mk
 
 # Soong
-include vendor/cygnus/configs/BoardConfigSoong.mk
+include vendor/cygnus/configs/BoardConfigSoong.mk 
+
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true) 
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
