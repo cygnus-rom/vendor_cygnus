031f468bc9e9fe675b4df3009e7165fdb7a84141
Shantanu Singh <singhshantanu555@gmail.com>
2020-03-13 16:23:15 +0100

    vendor: use prebuilt pixel bootanimation
    Credits:- @PixelExperience

--- configs/common.mk
+++ configs/common.mk
@@ -131,4 +131,17 @@ PRODUCT_PACKAGES += \
 	ViaBrowser
 
 # Bootanimation
-PRODUCT_PACKAGES += bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/cygnus/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/cygnus/bootanimation/bootanimation-dark_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/cygnus/bootanimation/bootanimation-dark_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "Cygnus: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Cygnus: Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/cygnus/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
