201e0196dc5f2525fd8557f1454b4467c742cffc
Ojas Sinha <sinhaojas67@gmail.com>
2020-06-06 16:40:36 +0200

    vendor: envsetup: new function to merge caf tags
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -2,6 +2,7 @@ function __print_cygnus_functions_help() {
 cat <<EOF
 Additional functions:
 - mka:             Builds using SCHED_BATCH on all processors.
+- merge:           Merge caf tags. Syntax- merge <caf-tag>
 EOF
 }
 
@@ -34,4 +35,65 @@ function mka() {
     fi
 
     schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+}
+
+function merge-caf() {
+    # Variables
+    root=$(gettop)
+    branch="caf-ten"
+    caf="https://source.codeaurora.org/quic/la"
+    read -p "Enter CAF tag: " tag
+
+    # Repos
+    repo="$(grep 'remote="cygnus"' $root/.repo/manifests/cygnus.xml  | awk '{print $2}' | awk -F '"' '{print $2}')"
+
+    # Repos that need to be excluded
+    blacklist="device/qcom/common \
+        manifest \
+        vendor/cygnus \
+        vendor/cygnus-overlays \
+        vendor/prebuilts"
+
+    # Create log files
+    rm $root/merge_log.txt
+    touch $root/merge_log.txt
+
+    # Reset and checkout to  remote's branch
+    reset() {
+        git reset --hard
+        git checkout $branch &> /dev/null
+    }
+
+    for repo in $repo; do
+        if [[ $blacklist =~ $repo ]]; then echo -e "Skipping $repo"
+        else
+        case $repo in
+            device/qcom/sepolicy)
+                repo_url="$caf/$repo" ;;
+            build/make)
+                repo_url="$caf/platform/build" ;;
+            vendor/codeaurora/commonsys/telephony)
+                repo_url="$caf/platform/vendor/codeaurora/telephony" ;;
+            vendor/qcom/opensource/commonsys/bluetooth)
+                repo_url="$caf/platform/vendor/qcom-opensource/bluetooth" ;;
+            vendor/qcom/opensource/commonsys/bluetooth_ext)
+                repo_url="$caf/platform/vendor/qcom-opensource/bluetooth_ext" ;;
+            vendor/qcom/opensource/commonsys/fm)
+                repo_url="$caf/platform/vendor/qcom-opensource/fm-commonsys" ;;
+            vendor/qcom/opensource/commonsys/system/bt)
+                repo_url="$caf/platform/vendor/qcom-opensource/system/bt" ;;
+            vendor/qcom/opensource/commonsys-intf/display)
+                repo_url="$caf/platform/vendor/qcom-opensource/display-commonsys-intf" ;;
+            vendor/qcom*) repo_url="$caf/platform/$(echo $repo | sed -e 's|qcom/opensource|qcom-opensource|')" ;;
+            *) repo_url="$caf/platform/$repo" ;; esac
+        fi
+
+        if wget -q --spider $repo_url; then
+            echo -e "Merging $repo"
+            cd $root/$repo
+            reset
+            git pull $repo_url $tag
+        fi
+        done
+    echo "Done"
 }
