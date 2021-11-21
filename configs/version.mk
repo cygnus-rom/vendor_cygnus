68c8320964507640db0764f5f0825d682a1e0a35
SparXFusion <s2234nadar@gmail.com>
2021-11-21 16:25:21 +0100

    vendor: Hail Cygnus 4.0!
    also change some flags for S
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- configs/version.mk
+++ configs/version.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 #
 
-CYGNUS_VERSION_MAJOR := 3
-CYGNUS_VERSION_MINOR := 5
+CYGNUS_VERSION_MAJOR := 4
+CYGNUS_VERSION_MINOR := 0
 CYGNUS_VERSION := $(CYGNUS_VERSION_MAJOR).$(CYGNUS_VERSION_MINOR)
 
 ifndef CYGNUS_BUILD_TYPE
-   CYGNUS_BUILD_TYPE := UNOFFICIAL
+   CYGNUS_BUILD_TYPE := BETA
 endif
 
 ifndef WITH_GAPPS
@@ -33,7 +33,7 @@ PACKAGE_VERSION := Cygnus-$(CYGNUS_VERSION)-$(CYGNUS_BUILD_TYPE)-$(CYGNUS_BUILD)
 MAIN_VERSION := Cygnus-$(CYGNUS_VERSION)-$(shell date +%m%d%H%M)
 CAF_REV := $(shell bash vendor/cygnus/configs/caf-tag.sh)
 
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
    ro.cygnus.version=$(CYGNUS_VERSION) \
    ro.cygnus.version.major=$(CYGNUS_VERSION_MAJOR) \
    ro.system.cygnus.build=$(MAIN_VERSION) \
